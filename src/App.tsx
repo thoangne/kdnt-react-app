8d826a9a2af265bd221a71ef9a670baf1d44d02f
vuongct-03 <vuongvuong2807@gmail.com>
2024-09-27 22:10:37 +0700

    tạo router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import InputForm from './components/InputForm';
 
+const HanderRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/login" element={<InputForm />} />
+
+  </Routes>
+);
 
 function App() {
   return (
     <>
-      <Home></Home>
+      <BrowserRouter>
+      <HanderRoutes />
+    </BrowserRouter>
     </>
   );
 }
