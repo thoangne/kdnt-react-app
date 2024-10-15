f6e9ad421314d9e4b3f5d1939b569b4de6dc4a4d
Thoangne <Nguythoang2003@gmail.com>
2024-10-15 21:43:14 +0700

    update

---
+++ src/layout/CheckOut.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import './CheckOut.scss'
+
+export const CheckOut = () => {
+  return (
+    <>
+        <Container>
+            <Row>
+                <Col>
+                    <h1 className="name-cart">Giỏ hàng của bạn</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+
+                </Col>
+            </Row>
+        </Container>
+    </>
+  )
+}
