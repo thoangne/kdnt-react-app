81db08d345e46f42a38518b9c8a26229398d22b6
Thoangne <Nguythoang2003@gmail.com>
2024-10-30 17:45:52 +0700

    UPDATE
    Them file PayOut.tsx PayOut.scss

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import AccountPage from './pages/AccountPage';
 import { FillerProduct } from './components/FillerProduct';
 import ShoppingCartPage from './pages/ShoppingCartPage';
 import { LoginPage } from './pages/LoginPage';
+import { PayOut } from './layout/PayOut';
 
 
 const HanderRoutes = () => (
@@ -21,7 +22,7 @@ const HanderRoutes = () => (
     <Route path="/register" element={<RegisterPage />} />
     <Route path="/product-detail/:productId" element={<ProductDetailPage />} />
     <Route path="/my-info" element={<AccountPage />} />
-    <Route path="/test" element={<ShoppingCartPage />} />
+    <Route path="/test" element={<PayOut />} />
 
 
   </Routes>
